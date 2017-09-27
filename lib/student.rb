92bcd724783085152a2fd7c22395b0a6941edeae
Carlota Bindner <cev.bindner@gmail.com>
2017-09-27 03:29:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,12 @@
-class Student
+class Student < Student
 
   def hello
+    super
     puts "Hey there! I'm so excited to learn stuff.\n"
   end
 
   def raise_hand
-    puts "Pick me!\n"
+    super*10
   end
 
 end
